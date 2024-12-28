12a3e8f7c7f0da282552e6933bafb834cd6a7894
intzam667 <yigitkaraman5140@gmail.com>
2024-12-29 00:47:13 +0300

    setup.bat

--- setup.bat
+++ setup.bat
@@ -1,18 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Check if the script is run as administrator
 openfiles >nul 2>nul
 if %errorlevel% neq 0 (
     echo Please run this script as administrator.
     exit /b 1
 )
 
-:: Define directories
 set USER_HOME=%USERPROFILE%
 set TARGET_DIR=%USER_HOME%\Documents\mpvtui667
 
-:: Create directory if it doesn't exist
 if not exist "!TARGET_DIR!" (
     mkdir "!TARGET_DIR!"
     if %errorlevel% equ 0 (
@@ -23,7 +20,6 @@ if not exist "!TARGET_DIR!" (
     )
 )
 
-:: Copy mpvtui.py to the target directory
 copy mpvtui.py "!TARGET_DIR!" >nul
 if %errorlevel% equ 0 (
     echo Copied mpvtui.py to !TARGET_DIR!
@@ -32,7 +28,6 @@ if %errorlevel% equ 0 (
     exit /b 1
 )
 
-:: Copy mpvtui.py to the target directory
 copy mpvtui.bat "!TARGET_DIR!" >nul
 if %errorlevel% equ 0 (
     echo Copied mpvtui.bat to !TARGET_DIR!
@@ -41,13 +36,10 @@ if %errorlevel% equ 0 (
     exit /b 1
 )
 
-:: Create a batch script to run the Python file
 echo @echo off > %USERPROFILE%\Documents\mpvtui667.bat
 echo python "%USERPROFILE%\Documents\mpvtui667\mpvtui.py" >> %USERPROFILE%\Documents\mpvtui667.bat
 
-:: Set file permissions (using icacls)
 icacls "%USERPROFILE%\Documents\mpvtui667.bat" /grant %USERNAME%:F >nul
-
 
 echo Don't forget to edit Path variable for "%USERPROFILE%\Documents\mpvtui667"
 
